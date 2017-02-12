d9162ef30502b160d874674d612fdfc6b56a6655
mopat <morczinietz@gmail.com>
2017-02-12 23:33:41 +0100

    download comstats with ophantom

---
+++ click.js
@@ -0,0 +1,29 @@
+var url = "http://www.comstats.de/matchday";
+var page = require('webpage').create();
+
+var fs = require('fs');
+var system = require('system');
+page.onConsoleMessage = function(msg, lineNum, sourceId) {
+    console.log('CONSOLE: ' + msg + ' (from line #' + lineNum + ' in "' + sourceId + '")');
+};
+page.onLoadFinished = function () {
+    console.log("page load finished");
+
+    // page.render('export.png');
+    page.render('results.png');
+    fs.write('results.html', page.content, 'a');
+};
+
+page.open(url, function (status) {
+    page.evaluate(function () {
+        var showResults = document.getElementsByClassName('detailsPlus');
+        for (var i = 0; i < showResults.length; i++) {
+            if (showResults[i] != undefined)
+                showResults[i].click();
+        }
+
+    }, function (result) {
+
+
+    });
+});
