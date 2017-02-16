e6872096057e649cdc972854a13b62ac587cbef5
mopat <morczinietz@gmail.com>
2017-02-16 20:36:23 +0100

    calc teampiunts

--- app/js/app.js
+++ app/js/app.js
@@ -4,6 +4,7 @@
 var API_URL = 'http://localhost:8000/';
 $(document).ready(function () {
     init();
+    getMyPlayers();
 });
 
 function init() {
@@ -39,4 +40,36 @@ function playerAutocomplete() {
             }
         )
     });
+}
+
+function getMyPlayers() {
+    var playersComunioIDs = ['32192', '32099', '32522', '32323', '27620', '31273'];
+    var squadPoints = 0;
+    for (var i = 0; i < playersComunioIDs.length; i++) {
+        $.ajax({
+            url: API_URL + "players?comunio_id=" + playersComunioIDs[i],
+            type: "get",
+            dataType: "json",
+            success: function (response) {
+                console.log(response.players[0]);
+                $('#results').empty();
+
+                var playerData = response.players[0];
+                var playerName = playerData.name;
+                var playerLastPoints = parseInt(playerData.last_points);
+                if (isNaN(playerLastPoints)) {
+                    playerLastPoints = 0;
+                }
+                squadPoints += playerLastPoints;
+                console.log(squadPoints);
+                var comunioID = playerData.comunio_id;
+                console.log(playerName, playerLastPoints);
+                $('#results').append("Teampunkte: " + squadPoints);
+
+            },
+            error: function (error) {
+
+            }
+        });
+    }
 }
